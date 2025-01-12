8370da87e806c71a59504658875593c5fe4b10da
shivendra0712 <patelshivendra817@gmail.com>
2025-01-12 15:39:36 +0530

    added logic

---
+++ script.js
@@ -0,0 +1,43 @@
+let fontname1 = document.querySelector(".fontname1");
+let variablestyles1 = document.querySelector(".variablestyles1");
+let fontheading1 = document.querySelector(".fontheading1");
+
+let fontname2 = document.querySelector(".fontname2");
+let variablestyles2 = document.querySelector(".variablestyles2");
+let fontheading2 = document.querySelector(".fontheading2");
+
+let fontname3 = document.querySelector(".fontname3");
+let variablestyles3 = document.querySelector(".variablestyles3");
+let fontheading3 = document.querySelector(".fontheading3");
+
+let fontname4 = document.querySelector(".fontname4");
+let variablestyles4 = document.querySelector(".variablestyles4");
+let fontheading4 = document.querySelector(".fontheading4");
+
+// console.log(fontname1.innerHTML);
+// console.log(variablestyles1.innerHTML);
+// console.log(fontheading1.innerHTML);
+
+fontname1.innerHTML = "Protest Revolution";
+fontheading1.innerHTML ="Protest Revolution";
+
+fontheading1.style.fontFamily ="Protest Revolution";
+variablestyles1.innerHTML ="5 styles";
+
+fontname2.innerHTML = "Margarine";
+fontheading2.innerHTML ="Margarine";
+
+fontheading2.style.fontFamily ="Margarine";
+variablestyles2.innerHTML ="5 styles";
+
+fontname3.innerHTML = "Playwrite VN";
+fontheading3.innerHTML ="Playwrite VN";
+
+fontheading3.style.fontFamily ="Playwrite VN";
+variablestyles3.innerHTML ="5 styles";
+
+fontname4.innerHTML = "Dancing Script";
+fontheading4.innerHTML ="Dancing Script";
+
+fontheading4.style.fontFamily ="Dancing Script";
+variablestyles4.innerHTML ="5 styles";
